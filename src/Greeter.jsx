4bd6623f4b87800fcb98a4e3764a4281c9cbc0fc
Bpost129 <Bpost129@yahoo.com>
2026-01-04 15:02:30 -0500

    render list of things with .map

--- src/Greeter.jsx
+++ src/Greeter.jsx
@@ -1,10 +1,16 @@
 function Greeter(props) {
+  const things = ["Water", "Cattle", "Gold"]
+  // Create an array of <li> components
+
   return (
-    <>
-      <h1>Hello, {props.friend ? props.friend.name : 'friend'}</h1>
+    <div>
+			<h1>Hello, {props.friend ? props.friend.name : "friend"}</h1>
       <h2>I believe in you.</h2>
-      <p>Today is {new Date().getDay() === 1 ? 'Monday' : 'not Monday'}</p>
-    </>
+      <p>Today is {new Date().getDay() === 1 ? "Monday" : "not Monday"}</p>
+      <h3>Give us your:</h3>
+      {/* Render the array of <li>s */}
+      <ul>{things.map(thing => <li key={thing}>{thing}</li>)}</ul>
+    </div>
   )
 }
 
