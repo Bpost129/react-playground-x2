184aad2fdbb87bf85c69e20b6ad77f607be9acea
Bpost129 <Bpost129@yahoo.com>
2026-01-04 15:29:14 -0500

    create, import, and render toy component for each toy per puppy

--- src/Puppy.jsx
+++ src/Puppy.jsx
@@ -1,7 +1,12 @@
+import Toy from "./Toy"
+
 const Puppy = (props) => {
   return (
     <>
       <h3>{props.puppy.name} is a {props.puppy.age} year old {props.puppy.breed}</h3>
+      {props.puppy.toys.map(toy =>
+        <Toy key={toy._id} puppyName={props.puppy.name} toy={toy} />
+      )}
     </>
   )
 }
---
+++ src/Toy.jsx
@@ -0,0 +1,9 @@
+const Toy = (props) => {
+  return (
+    <>
+      <h4>{props.puppyName} loves playing with a {props.toy}!</h4>
+    </>
+  )
+}
+
+export default Toy
