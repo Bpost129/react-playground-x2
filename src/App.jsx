bdf0bfa590621d6779450c53fde19981fae3aba5
Bpost129 <Bpost129@yahoo.com>
2026-01-04 15:06:42 -0500

    add puppy data to app, create puppy list component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,30 @@ import './App.css'
 import Greeter from './Greeter'
 
 function App() {
+  const puppies = [
+    {
+			_id: '82319f43fj2349jf9j',
+      name: 'Ruby', 
+      breed: 'Cheese Hound', 
+      age: 3, 
+      toys: ['squeaky ball', 'stick', 'giant rope toy']
+    },{
+			_id: '45892n34nv93n27v4',
+      name: 'Liam', 
+      breed: 'Schnauzer', 
+      age: 3, 
+      toys: ['green duck', 'purple duck', 'yellow duck']
+    },{
+			_id: '92013mcq1234243wd',
+      name: 'Taco', 
+      breed: 'Golden Retriever', 
+      age: 9, 
+      toys: ['deer antler', 'stuffed pig', 'squeaky duck']
+    }
+  ]
   return (
     <div>
-      <Greeter friend={{name: 'Wilma', town: 'Roswell'}} />
+
     </div>
   )
 }
---
+++ src/PuppyList.jsx
@@ -0,0 +1,9 @@
+const PuppyList = () => {
+  return (
+    <>
+    <h2>This is a puppy list component!</h2>
+    </>
+  )
+}
+
+export default PuppyList
