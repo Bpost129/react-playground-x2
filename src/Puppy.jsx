704242ab79a10730cb2024d0ffd2d9dad86da020
Bpost129 <Bpost129@yahoo.com>
2026-01-04 15:20:36 -0500

    create, import, and render puppy component within puppy list

---
+++ src/Puppy.jsx
@@ -0,0 +1,9 @@
+const Puppy = (props) => {
+  return (
+    <>
+      <h2>This is a puppy component</h2>
+    </>
+  )
+}
+
+export default Puppy
--- src/PuppyList.jsx
+++ src/PuppyList.jsx
@@ -1,7 +1,12 @@
-const PuppyList = () => {
+import Puppy from "./Puppy"
+
+const PuppyList = (props) => {
   return (
     <>
       <h2>This is a puppy list component!</h2>
+      {props.puppies.map(puppy => 
+        <Puppy key={puppy._id} puppy={puppy} />
+      )}
     </>
   )
 }
